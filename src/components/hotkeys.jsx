df363b74720f48c37665ced37a308f1baa618c20
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-10-27 20:33:29 +1030

    Space bar is kinda work rn lol

--- src/components/hotkeys.jsx
+++ src/components/hotkeys.jsx
@@ -1,20 +1,43 @@
 import { getGlobalEditor } from "../utils/editorContext";
 
+let isRunning = false;
+let count = 0
+
+
 function togglePlayStop() {
     const globalEditor = getGlobalEditor();
-    if (globalEditor) {
-        globalEditor.stop();
-    } 
-    else {
-        console.warn("Not ready to STOP");
+    if (!globalEditor) {
+        alert("No setting yet!")
     }
+
+    if (count === 0) {
+        globalEditor.stop()
+        count = count + 1
+        isRunning = false
+    }
+    else {
+        if (isRunning) {
+            globalEditor.stop();
+            isRunning = false
+        } 
+        else {
+            globalEditor.evaluate();
+            isRunning = true
+        }
+    }
+
 }
 
 export function SetupEffectHotkeys() {
     window.addEventListener("keydown", (e) => {
 
         switch (e.code) {
-            case "Space": togglePlayStop(); break;
+            case "Space": {
+                togglePlayStop()
+                e.preventDefault();
+                break;
+            }
+
             default: break;
         }
     });
