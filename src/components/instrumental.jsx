2f0913e3b5648d1b089bf20eac8454cafe7a1834
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-10-30 12:26:47 +1030

    Fix the code, turns out it not so complicated I was overthink

--- src/components/instrumental.jsx
+++ src/components/instrumental.jsx
@@ -1,22 +1,7 @@
 
-import { useState } from "react";
 import { Proc } from "../utils/proc";
 
 export default function Instrumental() {
-
-    const [instruments, setInstruments] = useState({
-        p1_radio: true,
-        p2_radio: true,
-        p3_radio: true,
-        p4_radio: true,
-    })
-
-    const toggle = (e) => {
-
-        const update = {...instruments, [e]: !instruments[e]} 
-        setInstruments(update)
-        Proc() // reevaluate the code
-    }
 
   return (
     <div className="instrumentals">
@@ -24,19 +9,19 @@ export default function Instrumental() {
         <div className="instrumental-grid">
             <div className="instrumental-items">
                 <label htmlFor="p1_radio">Bassline</label>
-                <input type="checkbox" id="p1_radio" onChange={() => toggle("p1_radio")} checked={instruments.p1_radio} />
+                <input type="checkbox" id="p1_radio" onChange={Proc} defaultChecked />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p2_radio">Main </label>
-                <input type="checkbox" id="p2_radio" onChange={() => toggle("p2_radio")} checked={instruments.p2_radio} />
+                <input type="checkbox" id="p2_radio" onChange={Proc} defaultChecked />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p3_radio">Drum 1</label>
-                <input type="checkbox" id="p3_radio" onChange={() => toggle("p3_radio")} checked={instruments.p3_radio} />
+                <input type="checkbox" id="p3_radio" onChange={Proc} defaultChecked />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p4_radio">Drum 2</label>
-                <input type="checkbox" id="p4_radio" onChange={() => toggle("p4_radio")} checked={instruments.p4_radio} />
+                <input type="checkbox" id="p4_radio" onChange={Proc} defaultChecked />
             </div>
         </div>
 
--- src/utils/proc.js
+++ src/utils/proc.js
@@ -12,4 +12,5 @@ export function Proc() {
     let proc_text = document.getElementById('proc').value
     let proc_text_replaced = ProcessText(proc_text)
     globalEditor.setCode(proc_text_replaced) 
+    globalEditor.evaluate()
 }
