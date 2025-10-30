7b98325878b5fe308d9b7b232353fae5cc618919
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-10-30 11:55:30 +1030

    Some styling for instrumentals

--- src/components/instrumental.jsx
+++ src/components/instrumental.jsx
@@ -5,23 +5,26 @@ export default function Instrumental() {
 
   return (
     <div className="instrumentals">
-        <h2>Instruments Panel</h2>
-            <div>
+        <h2 className="instrumentals-header">Instruments Panel</h2>
+        <div className="instrumental-grid">
+            <div className="instrumental-items">
                 <label htmlFor="p1_radio">Bassline</label>
                 <input type="checkbox" id="p1_radio" onChange={Proc} defaultChecked />
             </div>
-            <div>
+            <div className="instrumental-items">
                 <label htmlFor="p2_radio">Main </label>
                 <input type="checkbox" id="p2_radio" onChange={Proc} defaultChecked />
             </div>
-            <div>
+            <div className="instrumental-items">
                 <label htmlFor="p3_radio">Drum 1</label>
                 <input type="checkbox" id="p3_radio" onChange={Proc} defaultChecked />
             </div>
-            <div>
+            <div className="instrumental-items">
                 <label htmlFor="p4_radio">Drum 2</label>
                 <input type="checkbox" id="p4_radio" onChange={Proc} defaultChecked />
             </div>
+        </div>
+
     </div>
   );
 }
