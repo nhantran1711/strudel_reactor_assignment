46d0795224b0358a381d5c2ccccf85f167a018a7
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-10-30 12:11:51 +1030

    Some bug fixed for instrumental but kinda broke the code -.-.

--- src/components/instrumental.jsx
+++ src/components/instrumental.jsx
@@ -1,7 +1,22 @@
 
+import { useState } from "react";
 import { Proc } from "../utils/proc";
 
 export default function Instrumental() {
+
+    const [instruments, setInstruments] = useState({
+        p1_radio: true,
+        p2_radio: true,
+        p3_radio: true,
+        p4_radio: true,
+    })
+
+    const toggle = (e) => {
+
+        const update = {...instruments, [e]: !instruments[e]} 
+        setInstruments(update)
+        Proc() // reevaluate the code
+    }
 
   return (
     <div className="instrumentals">
@@ -9,19 +24,19 @@ export default function Instrumental() {
         <div className="instrumental-grid">
             <div className="instrumental-items">
                 <label htmlFor="p1_radio">Bassline</label>
-                <input type="checkbox" id="p1_radio" onChange={Proc} defaultChecked />
+                <input type="checkbox" id="p1_radio" onChange={() => toggle("p1_radio")} checked={instruments.p1_radio} />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p2_radio">Main </label>
-                <input type="checkbox" id="p2_radio" onChange={Proc} defaultChecked />
+                <input type="checkbox" id="p2_radio" onChange={() => toggle("p2_radio")} checked={instruments.p2_radio} />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p3_radio">Drum 1</label>
-                <input type="checkbox" id="p3_radio" onChange={Proc} defaultChecked />
+                <input type="checkbox" id="p3_radio" onChange={() => toggle("p3_radio")} checked={instruments.p3_radio} />
             </div>
             <div className="instrumental-items">
                 <label htmlFor="p4_radio">Drum 2</label>
-                <input type="checkbox" id="p4_radio" onChange={Proc} defaultChecked />
+                <input type="checkbox" id="p4_radio" onChange={() => toggle("p4_radio")} checked={instruments.p4_radio} />
             </div>
         </div>
 
